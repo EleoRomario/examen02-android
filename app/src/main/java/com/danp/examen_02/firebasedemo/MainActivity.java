0047912b46d8507f604dea9185460893b1ebe7ee
EleoRomario <eleo.romario@gmail.com>
2022-07-12 17:10:31 -0500

    se nos modifico el manifest y lo corregimos y listo

--- app/src/main/java/com/danp/examen_02/firebasedemo/MainActivity.java
+++ app/src/main/java/com/danp/examen_02/firebasedemo/MainActivity.java
@@ -1,17 +1,12 @@
 package com.danp.examen_02.firebasedemo;
 
-import android.content.Intent;
 import android.os.Bundle;
 import android.util.Log;
-import android.view.Menu;
-import android.view.MenuItem;
 
 import com.google.android.gms.tasks.OnCompleteListener;
 import com.google.android.gms.tasks.Task;
-import com.google.firebase.auth.FirebaseAuth;
 import com.google.firebase.iid.FirebaseInstanceId;
 import com.google.firebase.iid.InstanceIdResult;
-import com.google.firebase.messaging.FirebaseMessaging;
 
 import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
