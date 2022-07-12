c823c80dfb1ae437d42d8d0c52e87bc14ce82f87
Eleo Huayhua <eleo.romario@gmail.com>
2022-07-12 17:01:09 -0500

    init

---
+++ app/src/main/java/com/danp/examen_02/firebasedemo/MainActivity.java
@@ -0,0 +1,42 @@
+package com.danp.examen_02.firebasedemo;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.util.Log;
+import android.view.Menu;
+import android.view.MenuItem;
+
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.iid.FirebaseInstanceId;
+import com.google.firebase.iid.InstanceIdResult;
+import com.google.firebase.messaging.FirebaseMessaging;
+
+import androidx.annotation.NonNull;
+import androidx.appcompat.app.AppCompatActivity;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        getToken();
+    }
+
+    private void getToken() {
+        FirebaseInstanceId.getInstance().getInstanceId().addOnCompleteListener(new OnCompleteListener<InstanceIdResult>() {
+            @Override
+            public void onComplete(@NonNull Task<InstanceIdResult> task) {
+                if (!task.isSuccessful()) {
+                    Log.w("MainActivity", "error al obtener el token", task.getException());
+                    return;
+                }
+                String token = task.getResult().getToken();
+                Log.d("TOKEN", token);
+            }
+        });
+    }
+}
